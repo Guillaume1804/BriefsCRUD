bad1e916458d513097d9e4bec2999b652a1ccd70
Guillaume Martinez <guillaume.martinez.1804@gmail.com>
2024-06-14 11:58:09 +0200

    JWT fonctionnelle + mise en place de sécurité qui vérifie si l'utilisateur possédent le token demandé

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
 const port = 3000;
+
+const secretKey = 'mdp123';
 
 // Middleware pour parser le JSON
 app.use(express.json());
 
 let users = [];
 
+// Fonction pour initialiser les utilisateurs avec des mots de passe hachés
 const initializeUsers = async () => {
   users = [
     { id: 1, name: "Alice", password: await argon2.hash("password1") },
@@ -19,13 +23,16 @@ const initializeUsers = async () => {
 };
 
 // Route pour récupérer tous les utilisateurs
-app.get("/users", (req, res) => {
+app.get("/users", authenticateToken, (req, res) => {
   res.json(users);
 });
 
 // Route pour récupérer un utilisateur spécifique par ID
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", authenticateToken, (req, res) => {
   const userId = parseInt(req.params.id);
+  if (req.user.id !== userId) {
+    return res.status(403).json({ message: "Access forbidden" });
+  }
   const user = users.find((u) => u.id === userId);
   if (user) {
     res.json(user);
@@ -51,9 +58,12 @@ app.post("/users", async (req, res) => {
 });
 
 // Route pour mettre à jour un utilisateur existant
-app.put("/users/:id", async (req, res) => {
+app.put("/users/:id", authenticateToken, async (req, res) => {
   try {
     const userId = parseInt(req.params.id);
+    if (req.user.id !== userId) {
+      return res.status(403).json({ message: "Access forbidden" });
+    }
     const user = users.find((u) => u.id === userId);
     if (user) {
       user.name = req.body.name;
@@ -68,8 +78,11 @@ app.put("/users/:id", async (req, res) => {
 });
 
 // Route pour supprimer un utilisateur existant
-app.delete("/users/:id", (req, res) => {
+app.delete("/users/:id", authenticateToken, (req, res) => {
   const userId = parseInt(req.params.id);
+  if (req.user.id !== userId) {
+    return res.status(403).json({ message: "Access forbidden" });
+  }
   const userIndex = users.findIndex((u) => u.id === userId);
   if (userIndex !== -1) {
     users.splice(userIndex, 1);
@@ -78,6 +91,42 @@ app.delete("/users/:id", (req, res) => {
     res.status(404).json({ message: "User not found" });
   }
 });
+
+// Route pour se connecter et obtenir un token JWT
+app.post("/login", async (req, res) => {
+  const { name, password } = req.body;
+  const user = users.find((u) => u.name === name);
+  if (!user) {
+    return res.status(400).send("Cannot find user");
+  }
+
+  try {
+    if (await argon2.verify(user.password, password)) {
+      const accessToken = jwt.sign({name: user.name, id: user.id}, secretKey, {expiresIn: '1h'});
+      res.json({accessToken});
+    }
+    else {
+      return res.status(403).send("Invalid password");
+    }
+  }
+  catch {
+    res.status(500).send('Error during login')
+  }
+});
+
+
+// Middleware pour authentifier les tokens JWT
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
 
 const startServer = async () => {
   await initializeUsers();
